ac8d81a9384a01936e4e2b6d3110f908f4c2e520
parthK57 <parth.kolgiri32@gmail.com>
2023-01-17 17:00:56 +0530

    fat arrow and student obj created

---
+++ Introduction/fat-arrow.js
@@ -0,0 +1,9 @@
+const mlt = (a, b) => a * b;
+
+console.log(mlt(8, 7));
+
+const student = {
+  name: "parth",
+  age: 22,
+  passout: true,
+};
--- Introduction/first-app.js
+++ Introduction/first-app.js
@@ -1,1 +1,5 @@
 console.log("Hello world!");
+
+const fs = require("fs");
+
+fs.writeFileSync("nodeCode.js", "console.log(\"Written by node\")");
